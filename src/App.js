ea2afae8b4d1cfe5c4b596ffaae1611a1c79e26c
Mona Abu Sharkh <mona@Monas-MacBook-Air.local>
2021-06-07 20:07:56 +0300

    App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Cakes from "./products.js";
 
 function App() {
+  const CakeList = Cakes.map((Cake) => (
+    <div className="cake" key={Cake.id}>
+      <img alt={Cake.name} className="cake-image" src={Cake.image} />
+      <p className="text">{Cake.name}</p>
+      <p className="text">{Cake.price} KD</p>
+    </div>
+  ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <div>
+        <h1 className="text"> Bake and stay awake </h1>
+        <h4 className="text">We beat it,you eat it!</h4>
+        <p className="text">
+          Welcome to the best bakery where every ingredient is specifically
+          extracted from naturelands, so... GET READY to make delicious
+          memories.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img
+          alt="bakery shop"
+          src="https://oxitamins.com/wp-content/uploads/2021/04/somers_bakery-department-header.jpg"
+          id="shop-image"
+        />
+      </div>
+      <div className="list">{CakeList}</div>
     </div>
   );
 }
