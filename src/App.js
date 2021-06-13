e021d8eb5bf463b9a42b164d456fc487fbdf6eec
Mona Abu Sharkh <mona@Monas-MacBook-Air.local>
2021-06-13 17:43:01 +0300

    switched modes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,39 @@ import "./App.css";
 import Home from "./Components/Home";
 
 import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./styles";
+import { GlobalStyle, ThemeButton } from "./styles";
+// use states
+import { useState } from "react";
+
 const theme = {
-  mainColor: "black",
-  backgroundColor: "baby pink",
+  light: {
+    mainColor: "#293241",
+    backgroundColor: "#ffc8dd",
+  },
+  dark: {
+    mainColor: "#ffc8dd",
+    backgroundColor: "#293241",
+  },
 };
+
 function App() {
+  const [currentTheme, setTheme] = useState(theme.light);
+
+  const toggleTheme = () => {
+    currentTheme === theme.dark ? setTheme(theme.light) : setTheme(theme.dark);
+  };
+  const themeButtonText =
+    currentTheme === theme.dark ? "Light Mode" : "Dark Mode";
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Home />
-    </ThemeProvider>
+    <div>
+      <ThemeProvider theme={currentTheme}>
+        <GlobalStyle />
+        <div>
+          <ThemeButton onClick={toggleTheme}>{themeButtonText}</ThemeButton>
+          <Home />
+        </div>
+      </ThemeProvider>
+    </div>
   );
 }
 
--- src/styles.js
+++ src/styles.js
@@ -37,3 +37,11 @@ export const ProductWrapper = styled.div`
     font-size: 50;
   }
 `;
+export const ThemeButton = styled.button`
+  font-size: 1em;
+  margin: 1.25em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  background-color: ${(props) => props.theme.mainColor};
+  color: ${(props) => props.theme.backgroundColor};
+`;
